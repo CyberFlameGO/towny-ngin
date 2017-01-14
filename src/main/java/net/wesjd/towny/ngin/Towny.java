2ae7f50df5f510ca627d5d3130a1cc922202c5f7
WesJD <wesjavadev@gmail.com>
2017-01-14 17:43:54 -0500

    Fix NPE, clean some stuff up

--- src/main/java/net/wesjd/towny/ngin/Towny.java
+++ src/main/java/net/wesjd/towny/ngin/Towny.java
@@ -1,9 +1,10 @@
 package net.wesjd.towny.ngin;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.google.inject.*;
+import com.google.inject.name.Named;
 import net.wesjd.towny.ngin.listeners.JoinLeaveListener;
+import net.wesjd.towny.ngin.storage.GStorageModule;
+import net.wesjd.towny.ngin.storage.StorageFolder;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,11 +12,25 @@ import java.util.Arrays;
 
 public class Towny extends JavaPlugin {
 
-    private Injector injector;
+    private final Injector injector = Guice.createInjector(
+            new GStorageModule(),
+            new AbstractModule() {
+                @Override
+                protected void configure() {
+                    bind(Towny.class).toInstance(Towny.this);
+                }
+            }
+    );
+
+    static class Hello {
+        @Inject
+        @Named("players")
+        private StorageFolder folder;
+    }
 
     @Override
     public void onEnable() {
-        injector = Guice.createInjector(new TownyModule());
+        injector.getInstance(Hello.class);
         registerListeners(JoinLeaveListener.class);
     }
 
@@ -32,15 +47,6 @@ public class Towny extends JavaPlugin {
 
     public Injector getInjector() {
         return injector;
-    }
-
-    private class TownyModule extends AbstractModule {
-
-        @Override
-        protected void configure() {
-            bind(Towny.class).toInstance(Towny.this);
-        }
-
     }
 
 }
