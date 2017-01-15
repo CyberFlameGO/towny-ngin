ba1b893d9b365a87747ed11b7723352aa4af489b
TheMrGong <gongora656@gmail.com>
2017-01-14 19:25:01 -0500

    Created packer for most data types
    PackerStore now uses lazy instantiation

---
+++ src/main/java/net/wesjd/towny/ngin/storage/pack/impl/primitives/IntegerPacker.java
@@ -0,0 +1,19 @@
+package net.wesjd.towny.ngin.storage.pack.impl.primitives;
+
+import net.wesjd.towny.ngin.storage.pack.Packer;
+import org.msgpack.core.MessagePacker;
+import org.msgpack.core.MessageUnpacker;
+
+import java.io.IOException;
+
+public class IntegerPacker extends Packer<Integer> {
+    @Override
+    public void packup(Integer packing, MessagePacker packer) throws IOException {
+        packer.packInt(packing);
+    }
+
+    @Override
+    public Integer unbox(MessageUnpacker unpacker) throws IOException {
+        return unpacker.unpackInt();
+    }
+}
