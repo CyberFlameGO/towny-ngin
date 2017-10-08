ec4205d1e1aaee4f0ce48b7dd68c4bbbf5236ef8
WesJD <wesjavadev@gmail.com>
2017-10-08 15:14:26 -0400

    chat listener (updater test)

--- src/main/java/net/wesjd/towny/ngin/listeners/ChatListener.java
+++ src/main/java/net/wesjd/towny/ngin/listeners/ChatListener.java
@@ -1,9 +1,17 @@
 package net.wesjd.towny.ngin.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 /**
  * Handles ingame chat
  */
 public class ChatListener implements Listener {
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        e.setMessage("666");
+    }
+
 }
